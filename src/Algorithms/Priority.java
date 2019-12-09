18c2e90c96b6d775bafc4643440ecb30da455292
A_Aboelfotoh <58115154+ahero12317@users.noreply.github.com>
2019-12-09 15:49:02 +0200

    updated prio algorithm
    made so that process with lower prio number have higher priority 
    and added average waiting time and average turn around time

--- src/Algorithms/Priority.java
+++ src/Algorithms/Priority.java
@@ -1,8 +1,8 @@
 package Algorithms;
-import Processes.Process;
-import java.util.*;
 
 public class Priority extends Algorithm {
+<<<<<<< Updated upstream
+=======
 	int n;
 	String name;
 	int Atime;
@@ -22,7 +22,7 @@ class Mycomparator implements Comparator<Object>
 		Process p2=(Process)o2;
 		if(p1.getmArrivalTime()<p2.getmArrivalTime())
 			return (-1);
-		else if(p1.getmArrivalTime()==p2.getmArrivalTime() && p1.getmPriority()>p2.getmPriority())
+		else if(p1.getmArrivalTime()==p2.getmArrivalTime() && p1.getmPriority()<p2.getmPriority())
 			return (-1);
 		else
 			return (1);
@@ -30,26 +30,12 @@ class Mycomparator implements Comparator<Object>
 	
 }
 
+>>>>>>> Stashed changes
 	@Override
 	public void Simulate() {
-		TreeSet<Process> queue=new TreeSet<Process>(new Mycomparator());
-		System.out.println("enter no. of processes");
-		n=sc.nextInt();
-		for(int i=0;i<n;i++)
-		{
-			Process p=new Process();
-			System.out.println("enter process name, arrival time, burst time, prio");
-			name=sc1.nextLine();
-			Atime=sc.nextInt();
-			Btime=sc.nextInt();
-			prio=sc.nextInt();
-			p.setmName(name);
-			p.setmArrivalTime(Atime);
-			p.setmBurstTime(Btime);
-			p.setmPriority(prio);
-			queue.add(p);	
-		}
 		
+<<<<<<< Updated upstream
+=======
 		ArrayList<Process> result=new ArrayList<Process>();
 		Iterator<Process> it = queue.iterator();
 		Initial_time = ((Process)queue.first()).getmArrivalTime();
@@ -63,12 +49,22 @@ class Mycomparator implements Comparator<Object>
 			result.add(p1);
 			
 		}
+		int sumWT=0;
+		int sumTAT=0;
+		int avgWT=0;
+		int avgTAT=0;
 		System.out.println("\n\nProcess \t Burst Time \t Wait Time \t Turn Around Time   Priority \n");
 		for(int i=0;i<result.size();i++)
 		{
 			
-			
+			sumWT=sumWT+result.get(i).getmWaitingTime();
+			sumTAT=sumTAT+result.get(i).getmTurnAroundTime();
 			System.out.print("\n   "+result.get(i).getmName()+"\t\t   "+result.get(i).getmBurstTime()+"\t\t     "+result.get(i).getmWaitingTime()+"\t\t     "+result.get(i).getmTurnAroundTime()+"\t\t     "+result.get(i).getmPriority()+"\n");
 		}
+	avgWT=sumWT/n;
+	avgTAT=sumTAT/n;
+	System.out.print("average waiting time: " + avgWT + "\n");
+	System.out.print("average turn around time: " + avgTAT + "\n");
+>>>>>>> Stashed changes
 	}
 }
