e1585b95767fe5a84c67150e0ee6b59ae2b3236d
BouSenna <sara_usa_1999@hotmail.com>
2019-12-12 20:33:41 +0200

    Added getInput function that takes the info of each process

--- src/Algorithms/AG.java
+++ src/Algorithms/AG.java
@@ -1,6 +1,68 @@
 package Algorithms;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import Processes.Process;
+
 public class AG extends Algorithm {
+	Scanner mInput;
+	ArrayList<Process> mProcesses;
+	ArrayList<Process> mTemp;
+	ArrayList<Process> mOutput;
+	ArrayList<Process> mDeadList;
+	Queue<Process> readyQueue;
+	int mMaxTime;
+	int mTotalQuantum;
+
+	public AG() {
+		this.mInput = new Scanner(System.in);
+		this.mProcesses = new ArrayList<>();
+		this.mTemp = new ArrayList<>();
+		this.mOutput = new ArrayList<>();
+		this.mDeadList = new ArrayList<>();
+		this.readyQueue = new LinkedList<>();
+		this.mMaxTime = 0;
+		this.mTotalQuantum = 0;
+
+		getInput();
+	}
+
+	public void getInput() {
+		int numOfProcesses;
+		System.out.println("Enter number of process.");
+		numOfProcesses = this.mInput.nextInt();
+
+		for (int i = 0; i < numOfProcesses; i++) {
+			System.out.println("Enter process " + i + " name.");
+			String processName = this.mInput.next();
+			System.out.println("Enter arrival Time of process " + i + ".");
+			int processAT = this.mInput.nextInt();
+			System.out.println("Enter burst Time of process " + i + ".");
+			int processBT = this.mInput.nextInt();
+			this.mMaxTime += processBT;
+			System.out.println("Enter quantum Time of process " + i + ".");
+			int processQT = this.mInput.nextInt();
+			this.mTotalQuantum += processQT;
+			System.out.println("Enter priority of process " + i + ".");
+			int processPriority = this.mInput.nextInt();
+			System.out.println("Enter process " + i + " colour.");
+			String processColour = this.mInput.next();
+			System.out.print("\n");
+
+			Process p = new Process(processName, processAT, processBT, processColour, processQT, processPriority);
+			p.setmAGFactor(processAT + processBT + processPriority);
+			p.setmRemainingTime(processBT);
+			this.mProcesses.add(p);
+			this.mTemp.add(p);
+
+			if (this.mMaxTime < processBT + processAT)
+				this.mMaxTime = processBT + processAT;
+		}
+	}
+	
 	@Override
 	public void Simulate() {
 		
